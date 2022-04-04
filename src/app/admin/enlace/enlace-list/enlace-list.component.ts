e3ada7395f54ba03fb52b536420f842b8ef3b971
Jossep Celmi <jcelmi157@gmail.com>
2022-04-04 17:37:05 -0500

    cambios enviados Diego

--- src/app/admin/enlace/enlace-list/enlace-list.component.ts
+++ src/app/admin/enlace/enlace-list/enlace-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EnlaceService } from 'src/app/services/enlace.service';
 
 @Component({
   selector: 'app-enlace-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./enlace-list.component.css']
 })
 export class EnlaceListComponent implements OnInit {
+  enlaces : any = [] 
 
-  constructor() { }
+  constructor(
+    private readonly ens: EnlaceService
+  ) { }
+
+  __getEnlaces(){
+    this.ens.__getEnlace().subscribe((rest: any) => {
+      this.enlaces = rest.data;
+      console.log(this.enlaces)
+    })
+  }
 
   ngOnInit(): void {
+    this.__getEnlaces();
   }
 
 }
